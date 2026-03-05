2bcfe0919d93d178d635233b32b20907e804701b
Tiago <tiagostrey@gmail.com>
2026-03-05 15:47:08 -0300

    feat: adiciona caso de uso e rota get para buscar assinatura por cpf

--- servico-gestao/src/infrastructure/modules/assinaturas.module.ts
+++ servico-gestao/src/infrastructure/modules/assinaturas.module.ts
@@ -4,8 +4,9 @@ import { AssinaturaSchema } from '../database/schemas/assinatura.schema';
 import { AssinaturaRepository } from '../database/repositories/assinatura.repository';
 import { AssinaturaController } from '../../interface/controllers/assinatura.controller';
 import { CriarAssinaturaUseCase } from '../../application/use-cases/criar-assinatura.use-case';
-import { ClientesModule } from './clientes.module'; // Importando para ter acesso ao repositório
-import { PlanosModule } from './planos.module';     // Importando para ter acesso ao repositório
+import { BuscarAssinaturasPorCpfUseCase } from '../../application/use-cases/buscar-assinaturas-por-cpf.use-case';
+import { ClientesModule } from './clientes.module';
+import { PlanosModule } from './planos.module';
 
 @Module({
     imports: [
@@ -16,6 +17,7 @@ import { PlanosModule } from './planos.module';     // Importando para ter acess
     controllers: [AssinaturaController],
     providers: [
         CriarAssinaturaUseCase,
+        BuscarAssinaturasPorCpfUseCase,
         {
             provide: 'IAssinaturaRepository',
             useClass: AssinaturaRepository,
--- servico-gestao/src/interface/Controllers/assinatura.controller.ts
+++ servico-gestao/src/interface/Controllers/assinatura.controller.ts
@@ -1,14 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CriarAssinaturaUseCase, CriarAssinaturaDto } from '../../application/use-cases/criar-assinatura.use-case';
+import { BuscarAssinaturasPorCpfUseCase } from '../../application/use-cases/buscar-assinaturas-por-cpf.use-case';
 
 @Controller('assinaturas')
 export class AssinaturaController {
     constructor(
         private readonly criarAssinaturaUseCase: CriarAssinaturaUseCase,
+        private readonly buscarAssinaturasPorCpfUseCase: BuscarAssinaturasPorCpfUseCase,
     ) {}
 
     @Post()
     async criar(@Body() dados: CriarAssinaturaDto) {
         return await this.criarAssinaturaUseCase.executar(dados);
+    }
+
+    @Get(':cpf')
+    async buscarPorCpf(@Param('cpf') cpf: string) {
+        return await this.buscarAssinaturasPorCpfUseCase.executar(cpf);
     }
 }
